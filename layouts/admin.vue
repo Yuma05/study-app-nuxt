8a7b1a560fa3ef1f85436a7a8efbbc292832891d
Yuma Ishii <yuuma1403@gmail.com>
2021-05-07 17:48:01 +0900

    Add temporary logout

---
+++ layouts/admin.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" temporary fixed app>
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title v-text="item.title" />
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item>
+          <v-btn
+            color="blue"
+            outlined
+            small
+            class="mx-auto"
+            @click="$store.dispatch('auth/logout')"
+          >
+            Logout
+          </v-btn>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar fixed color="white" app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title v-text="title" />
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      items: [
+        {
+          icon: 'mdi-apps',
+          title: 'Welcome',
+          to: '/',
+        },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+      ],
+      title: 'Study app',
+    }
+  },
+}
+</script>
