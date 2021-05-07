986c953738bb2439acba694b387ef212fbd97353
Yuma Ishii <yuuma1403@gmail.com>
2021-05-07 16:45:34 +0900

    Delete footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -27,9 +27,6 @@
         <nuxt />
       </v-container>
     </v-main>
-    <v-footer :absolute="!fixed" app>
-      <span>&copy; {{ new Date().getFullYear() }}</span>
-    </v-footer>
   </v-app>
 </template>
 
