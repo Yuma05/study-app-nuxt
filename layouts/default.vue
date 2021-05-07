cc608170f6455a20b8509064e1781124248727ee
Yuma Ishii <yuuma1403@gmail.com>
2021-05-07 16:45:11 +0900

    Add app attribute

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <v-app>
-    <v-navigation-drawer v-model="drawer" temporary fixed>
+    <v-navigation-drawer v-model="drawer" temporary fixed app>
       <v-list>
         <v-list-item
           v-for="(item, i) in items"
@@ -18,7 +18,7 @@
         </v-list-item>
       </v-list>
     </v-navigation-drawer>
-    <v-app-bar fixed color="white">
+    <v-app-bar fixed color="white" app>
       <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
       <v-toolbar-title v-text="title" />
     </v-app-bar>
