d87f3f2d730635c704ecdc84ef95441bbf1d66d9
Yuma Ishii <yuuma1403@gmail.com>
2021-05-07 18:20:40 +0900

    Set API URL to an environment variable

--- services/AuthService.js
+++ services/AuthService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const authClient = axios.create({
-  baseURL: 'http://localhost',
+  baseURL: process.env.API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
