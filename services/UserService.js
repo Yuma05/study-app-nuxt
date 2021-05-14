f3cc053dba575ff6a088e9390916c38b20e17d92
Yuma Ishii <yuuma1403@gmail.com>
2021-05-14 16:38:24 +0900

    Add api requests

--- services/UserService.js
+++ services/UserService.js
@@ -4,7 +4,13 @@ export default {
   getCategory() {
     return API.apiClient.get('/category')
   },
+  getOneCategory(id) {
+    return API.apiClient.get('/category/' + id)
+  },
   getStatus() {
     return API.apiClient.get('/status')
+  },
+  getMaterial(categoryId) {
+    return API.apiClient.get('/material/' + categoryId)
   },
 }
